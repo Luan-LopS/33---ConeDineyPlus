0d67c28cb34aee76c8a5c5df39915ad65b45f59e
Luan <luam10@hotmail.com.br>
2024-09-24 14:35:29 -0300

    ajuste no mobile

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]')
+    const questions = document.querySelectorAll('[data-faq-question]')
+    const heroSection = document.querySelector('.hero')
+    const alturaHero = heroSection.clientHeight
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY
+        if(posicaoAtual < alturaHero){
+            ocultaHeader()
+            
+        }else{
+            exibeHeader()
+        }
+    })
 
 
     for(let i = 0; i < buttons.length; i++){
@@ -12,7 +25,32 @@ document.addEventListener('DOMContentLoaded', function(){
             botao.target.classList.add('shows__tabs__button--is-active')
         })
     }
+
+
+    for(let i = 0; i<questions.length; i++){
+        questions[i].addEventListener('click',abreOUFechaResposta)
+    }
 })
+
+function ocultaHeader(){
+    const header = document.querySelector('header')
+    header.classList.add('header--is-hidden')
+}
+
+function exibeHeader(){
+    const header = document.querySelector('header')
+    header.classList.remove('header--is-hidden')
+}
+
+
+function abreOUFechaResposta(elemento){
+    const classe = 'faq__questions__item--is-open'
+    const elementoPai = elemento.target.parentNode
+
+    elementoPai.classList.toggle(classe)
+
+
+}
 
 function removeBotao(){
     const buttons = document.querySelectorAll('[data-tab-button]')
@@ -27,3 +65,4 @@ function escondeTodasAbas(){
         tabsContainer[i].classList.remove('shows__list--is-active')
     }
 }
+
